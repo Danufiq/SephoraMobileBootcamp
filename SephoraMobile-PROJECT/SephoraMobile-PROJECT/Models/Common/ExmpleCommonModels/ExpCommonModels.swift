1d431b0c2d3168e975aca6beb395c9a65c5f9563
dana nur fiqi <danufiq98@gmail.com>
2022-11-19 08:35:46 +0700

    new file

---
+++ SephoraMobile-PROJECT/SephoraMobile-PROJECT/Models/Common/ExmpleCommonModels/ExpCommonModels.swift
@@ -0,0 +1,9 @@
+//
+//  ExpCommonModels.swift
+//  SephoraMobile-PROJECT
+//
+//  Created by dana nur fiqi on 19/11/22.
+//  Copyright © 2022 Key. All rights reserved.
+//
+
+import Foundation
---
+++ SephoraMobile-PROJECT/SephoraMobile-PROJECT/Models/Main/ExampleMain/ExampleMainModels.swift
@@ -0,0 +1,9 @@
+//
+//  ExampleMainModels.swift
+//  SephoraMobile-PROJECT
+//
+//  Created by dana nur fiqi on 19/11/22.
+//  Copyright © 2022 Key. All rights reserved.
+//
+
+import Foundation
